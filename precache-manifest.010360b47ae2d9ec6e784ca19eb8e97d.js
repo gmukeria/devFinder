209d5fc22bbc16078e5e1a7de0b43da2fd091959
giorgi <giorgi.muqeria@gmail.com>
2022-12-01 14:00:37 +0400

    Updates

---
+++ precache-manifest.010360b47ae2d9ec6e784ca19eb8e97d.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "7a97cf2af733955d831f",
+    "url": "/devFinder/static/css/main.78e41206.chunk.css"
+  },
+  {
+    "revision": "7a97cf2af733955d831f",
+    "url": "/devFinder/static/js/main.7a97cf2a.chunk.js"
+  },
+  {
+    "revision": "ae4700ead3b6e1632dda",
+    "url": "/devFinder/static/js/1.ae4700ea.chunk.js"
+  },
+  {
+    "revision": "55532cbaef3edb684451",
+    "url": "/devFinder/static/js/2.55532cba.chunk.js"
+  },
+  {
+    "revision": "8228c3674140f32047b0",
+    "url": "/devFinder/static/js/runtime~main.8228c367.js"
+  },
+  {
+    "revision": "062faca2a09f52936088587dae9f6de1",
+    "url": "/devFinder/static/media/icon-moon.062faca2.svg"
+  },
+  {
+    "revision": "a7eda119898846c08529bfb44e6b9068",
+    "url": "/devFinder/static/media/avatar.a7eda119.jpeg"
+  },
+  {
+    "revision": "4e1e65c47abe57b0fdfb19144089d7d9",
+    "url": "/devFinder/static/media/icon-twitter.4e1e65c4.svg"
+  },
+  {
+    "revision": "b433e22d01073904c774a3aab11bfab0",
+    "url": "/devFinder/static/media/icon-location.b433e22d.svg"
+  },
+  {
+    "revision": "547abbf4352e547b4f80658c370b6c43",
+    "url": "/devFinder/static/media/icon-website.547abbf4.svg"
+  },
+  {
+    "revision": "64e9c16d84797ff56142e84957b6a212",
+    "url": "/devFinder/static/media/icon-company.64e9c16d.svg"
+  },
+  {
+    "revision": "d4dcb16294ad206b5627a65ed7f5fff4",
+    "url": "/devFinder/static/media/icon-search.d4dcb162.svg"
+  },
+  {
+    "revision": "b65af46b959d045d21e7e42374b9af09",
+    "url": "/devFinder/index.html"
+  }
+];
